cd229d2d5c5ceb31248e3a4d1e4039a89ae23bf2
GET Academy <terje@kolderup.net>
2021-06-25 09:39:27 +0200

    Initial commit

---
+++ InheritanceAndInterface/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace InheritanceAndInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamDemo.Run();
+            /*
+               - Interface 
+                    - kontrakt
+                    - dependency inversion
+                    - eks: 
+                        - PC-komponenter, WC, strøm, lyspære
+                        - bygge en tjeneste uavhengig av konkrete underliggende tjenester 
+                          summere tall fra fil - eller tastatur - eller fra nettverk
+               - Arv
+                    - "is a"
+                    - en relasjon fra virkeligheten
+                    - mer generelt/abstrakt i toppen av arvehierarkiet
+                    - mer konkret i bunnen av arvehierarkiet
+                    - eks: 
+                        - menneske : Primat : Pattedyr : Ryggstrengdyr : Dyr : Organisme
+                          https://no.wikipedia.org/wiki/Menneske
+                        - streams, enten se på innebygde eller lagen egen som leser bytes + arve 
+                          for mer spesifikt - feks. PointReader og TextLineReader
+                        - brukergrensesnittkomponenter
+                        - HvetebolleOppskrift : GjærbakstOppskrift : BakeOppskrift : Oppskrift
+                - Composition
+                    - "has a"
+                    - eks:  class House { 
+                                Door[] Doors;    
+                            }
+             */
+        }
+    }
+}
---
+++ InheritanceAndInterface/StreamDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAndInterface
+{
+    class StreamDemo
+    {
+        public static void Run()
+        {
+            var webClient = new WebClient();
+            var stream = webClient.OpenRead("https://www.getacademy.no");
+        }
+    }
+}
